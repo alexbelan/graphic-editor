9020d33583f118f619650520507a4662bed8bac9
Ivan Kazantsev <io.kazantcev@ya.ru>
2023-08-25 11:03:56 +0300

    feat(store): Добавлен Redux-toolkit

--- src/app/store/store.tsx
+++ src/app/store/store.tsx
@@ -2,3 +2,16 @@
 // export type RootState = ReturnType<typeof appStore.getState>;
 // // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 // export type AppDispatch = typeof appStore.dispatch;
+
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./rootReducer.tsx";
+
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
